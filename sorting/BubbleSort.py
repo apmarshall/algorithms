52e45acc3ba2993ce6e02ec4d08bcc8d7b29f4fa
Alex Floyd Marshall <apmarshall@soren.tech>
2021-03-29 06:04:30 -0400

    Updates to BubbleSort
    `NeedSort` variable made global in scope
    Helper functions un-nested from main function

--- sorting/BubbleSort.py
+++ sorting/BubbleSort.py
@@ -1,5 +1,6 @@
+needSort = true
+
 def bubbleSort ( list ):
-    needSort = true
     def listSort ( list, slist ):
         if ( list.length <= 1 || needSort == false ):
             return list + slist
@@ -10,14 +11,14 @@ def bubbleSort ( list ):
             listSort( list[:list.length], slist )
     listSort( list, [] )
 
-    def listIteration ( list )
-        if ( list.length <= 1 ): return list
-        else:
-            list[0,1] = tupleSort( list[0], list[1] )
-            listIteration( list[1:] )
+def listIteration ( list )
+   if ( list.length <= 1 ): return list
+   else:
+         list[0,1] = tupleSort( list[0], list[1] )
+         listIteration( list[1:] )
 
-    def tupleSort ( x, y ):
-        if ( x < y ): return ( x, y )
-        else:
-            needSort = true
-            return ( y, x )
+def tupleSort ( x, y ):
+   if ( x < y ): return ( x, y )
+   else:
+      needSort = true
+      return ( y, x )
