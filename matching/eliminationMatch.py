fef4c9d509ed07e32d2f40a9d8e6531756aa9e50
Alex Floyd Marshall <apmarshall@soren.tech>
2021-03-31 06:02:38 -0400

    Updates to elimination match recursive implementation

--- matching/eliminationMatch.py
+++ matching/eliminationMatch.py
@@ -1,20 +1,17 @@
 def elimination_match(M, A=None):
     if A is None:
         A = set(range(len(M)))
-    def B_construct:
-        B = [0*len(A)] # Rewrite
-        for M[i] in A:
-            B[i] += 1
-    def B_reduction():
-        # Reduce counts in B based on previous iteration
-    def list_reduction(L, C):
-        L.remove(C.pop())
+    B = [0]*len(A)
+    for [i] in M:
+        B[i] += 1
     def elimination_match_priv(M, A, B):
         if len(A) == 1: return A
         else:
-            C = indexOf(0 in B) # Rewrite
-            B = B_reduction(B, C, M)
-            A = list_reduction(A, C)
-            B = list_reduction(B, C)
+            C = indexOf(0 in B) # rewrite
+            j = M(C.pop())
+            B[j] -= 1
+            A.remove[C]
+            B.remove[C]
+            M.remove[C]
             return elimination_match_priv(M, A, B)
     return elimination_match_priv(M, A, B)
